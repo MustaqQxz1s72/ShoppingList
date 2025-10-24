603209549927212ad862644f156f27b98fee120d
MustaqQxz1s72 <mustaqahamadhp@gmail.com>
2025-10-24 13:11:21 +0200

    Unit test modification

--- src/main/java/com/example/shoppinglist/ShoppingListManagerApplication.java
+++ src/main/java/com/example/shoppinglist/ShoppingListManagerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoppinglistApplication {
+public class ShoppingListManagerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ShoppinglistApplication.class, args);
+        SpringApplication.run(ShoppingListManagerApplication.class, args);
     }
 }
--- src/test/java/com/example/ShoppingListManager/ShoppingListManagerApplicationTests.java
+++ src/test/java/com/example/ShoppingListManager/ShoppingListManagerApplicationTests.java
@@ -2,12 +2,12 @@ package com.example.ShoppingListManager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.shoppinglist.ShoppingListManagerApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = ShoppingListManagerApplication.class)
 class ShoppingListManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
